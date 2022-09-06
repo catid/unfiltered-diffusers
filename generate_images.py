b570920f8e8fcdc49f8c82fbf933c0b82d8dc7ec
catid <mrcatid@gmail.com>
2022-09-06 16:42:35 -0700

    wip

---
+++ generate_images.py
@@ -0,0 +1,42 @@
+# make sure you're logged in with `huggingface-cli login`
+from torch import autocast
+import torch
+from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
+import time
+
+lms = LMSDiscreteScheduler(
+    beta_start=0.00085, 
+    beta_end=0.012, 
+    beta_schedule="scaled_linear"
+)
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "CompVis/stable-diffusion-v1-4", 
+    scheduler=lms,
+    use_auth_token=True
+)
+pipe = pipe.to("cuda")
+
+prompt = "sexy bird posing nude"
+
+
+seed = 18736684
+steps = 60
+h = 512
+w = 512
+batch = 6
+
+for j in range(1, 100):
+
+    seed_n = seed + j
+
+    start = time.time()
+    generator = torch.Generator("cuda").manual_seed(seed_n)
+    with autocast("cuda"):
+        images = pipe([prompt] * batch, num_inference_steps=steps, height=h, width=w, generator=generator)["sample"]
+   
+        for i, img in enumerate(images):
+            img.save("{}_{}_{}.png".format(prompt, seed_n, i)) 
+
+    end = time.time()
+    print("Time to complete {} images: {} seconds".format(batch, end - start))
---
+++ unfiltered_readme.md
@@ -0,0 +1,19 @@
+# Unfiltered version
+
+This removes the NSFW filter to improve performance of image generation when used in batch mode.
+
+## How-To
+
+Run:
+
+```
+pip install .
+```
+
+I also had to install pytorch locally with pip: https://pytorch.org/get-started/locally/
+
+Then you can generate images:
+
+```
+python3 generate_images.py
+```
